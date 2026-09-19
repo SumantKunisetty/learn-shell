8081eec00f71e62cf0ac11677729f8fe96640d2b
Sumant-gituser <sumant@git.com>
2026-09-19 20:35:52 +0530

    auth-update9

--- auth-service.sh
+++ auth-service.sh
@@ -1,20 +1,14 @@
 dnf install -y golang
-
 cp auth-service.service /etc/systemd/system/
 useradd -r -s /bin/false appuser
 mkdir -p /app
-echo "download"
 curl -L -o /tmp/auth-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/auth-service.tar.gz
-echo "completed"
 cd /app
-echo "move"
-pwd
 tar xzf /tmp/auth-service.tar.gz
-ls -latr
-echo "extracted"
+cd /app
+CGO_ENABLED=0 go build -o auth-service ./cmd/server
 chown -R appuser:appuser /app
 chmod o-rwx /app -R
-
 systemctl daemon-reload
 systemctl enable auth-service
 systemctl start auth-service
