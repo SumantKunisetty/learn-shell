c02bc97e070cba27dade7ecf07507db50d82d173
Sumant-gituser <sumant@git.com>
2026-09-19 19:56:35 +0530

    auth-update3

--- auth-service.sh
+++ auth-service.sh
@@ -1,6 +1,6 @@
 dnf install -y golang
 go version
-cp auth-service.sh /etc/systemd/system/
+cp auth-service.service /etc/systemd/system/
 useradd -r -s /bin/false appuser
 mkdir -p /app
 curl -L -o /tmp/auth-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/auth-service.tar.gz
